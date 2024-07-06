a6e33643d39479f427fb7c162e414f75d47126ab
pikun <pikun@meituan.com>
2024-07-06 13:35:53 +0800

    Deploy website - based on d9c44eb549b480249215740e026f7dcf5cc4ad76

---
+++ analytics.js
@@ -0,0 +1,8 @@
+(function () {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-HJDLJ8DZT3'); // 替换为你的跟踪 ID
+})();
